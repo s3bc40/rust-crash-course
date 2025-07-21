bb2592f30493fc7cc94cb1a7fe74216ede5ecfd8
s3bc40 <s3bc40@proton.me>
2025-07-21 10:15:07 +0200

    feat: hello exercise

--- topics/hello/exercises/src/main.rs
+++ topics/hello/exercises/src/main.rs
@@ -4,6 +4,9 @@
 #![allow(unused)]
 
 // main() is the entry point of Rust program
+fn main() {
+    println!("Hello World!")
+}
 
 // println! is a macro that prints text to the console.
 // Macros in Rust generate code at compile time and are
